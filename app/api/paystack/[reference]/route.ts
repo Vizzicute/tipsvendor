869e9829159e5695b7637f536b65c483923cb8ff
Vic Octavian <victoctavian124@gmail.com>
2025-06-19 20:19:15 +0100

    unused import removed

--- app/api/paystack/[reference]/route.ts
+++ app/api/paystack/[reference]/route.ts
@@ -1,5 +1,5 @@
 import { getWalletSettings } from "@/lib/appwrite/appConfig";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(
   { params }: { params: { reference: string } }
