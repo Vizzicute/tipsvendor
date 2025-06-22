026f786ce9975f04834527656af3c5eef05df268
Vic Octavian <victoctavian124@gmail.com>
2025-06-23 00:54:57 +0100

    Enhances sitemap generation with SEO pages integration

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,129 +1,40 @@
-import { getBlog, getBlogCategories } from "@/lib/appwrite/fetch";
+import { getBlog, getBlogCategories, getSeoPages } from "@/lib/appwrite/fetch";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const blogs = await getBlog();
-    const blogCategories = await getBlogCategories();
-    return [
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}`,
-            priority: 1,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/login`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/register`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/about`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/banker`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/blog`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/btts`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/chance`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/draw`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/either`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/htft`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/info`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/livescore`,
-            priority: 0.6,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/over1`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/over2`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/over3`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/over4`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/under4`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/under3`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/under2`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/under1`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        {
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/privacy`,
-            priority: 0.4,
-            lastModified: Date()
-        },
-        ...(blogs?.map((blog) => ({
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/blog/${blog.slug}`,
-            priority: 0.5,
-            lastModified: Date()
-        })) || []),
-        ...(blogCategories?.map((category) => ({
-            url: `${process.env.NEXT_PUBLIC_APP_URL}/blog/category/${category.slug}`,
-            priority: 0.5,
-            lastModified: Date()
-        })) || [])
-    ]
+  const blogs = await getBlog();
+  const blogCategories = await getBlogCategories();
+  const seoPages = await getSeoPages();
+
+  const filteredPages = seoPages?.filter(
+    (page) =>
+      page.url !== "" ||
+      page.url !== "privacy" ||
+      page.url !== "info" ||
+      page.url !== "about"
+  );
+  const homepage = seoPages?.find((page) => page.url === "");
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}`,
+      priority: 1,
+      lastModified: new Date(homepage?.$updatedAt || ""),
+    },
+    ...(filteredPages?.map((page) => ({
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/${page.url}`,
+      priority: 0.6,
+      lastModified: new Date(page.$updatedAt),
+    })) || []),
+    ...(blogs?.map((blog) => ({
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/blog/${blog.slug}`,
+      priority: 0.5,
+      lastModified: new Date(blog.$updatedAt),
+    })) || []),
+    ...(blogCategories?.map((category) => ({
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/blog/category/${category.slug}`,
+      priority: 0.5,
+      lastModified: new Date(category.$updatedAt),
+    })) || []),
+  ];
 }
